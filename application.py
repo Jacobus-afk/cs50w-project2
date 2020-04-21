2f2c87535d490b4471c932f99675ff84f8feb9a5
Cobus <cobus@moduteq.com>
2020-04-22 00:18:36 +0200

    channel creation and check for existing channel

--- application.py
+++ application.py
@@ -1,13 +1,24 @@
 import os
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channel_list = {}
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@socketio.on("create channel")
+def create_channel(data):
+    channelname = data["channel_name"]
+    if channelname in channel_list:
+        emit("resp", {"reason": "channel already exists"})
+    else:
+        channel_list[channelname] = data
+        emit("resp", {"reason": f"channel {channelname} added"})
+
--- static/index.js
+++ static/index.js
@@ -1,17 +1,21 @@
 let handle = localStorage.getItem('user_handle');
 
-
 document.addEventListener("DOMContentLoaded", () => {
+    const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    socket.on('connect', () => {
+        console.log("socket connected")
+    });
+
     const new_user_div = document.getElementById("new-user");
     const old_user_div = document.getElementById("old-user");
 
-    if (!handle) {
-        new_user_div.style.display = "block";
-  
-        const new_form = document.querySelector("#new-user-form");
-        const name = new_form.querySelector('input[name="handle"]');
-        const button = new_form.querySelector('input[type="submit"]');
-        
+    const forms = document.forms;
+    
+    for (i = 0; i < forms.length; i++)
+    {
+        const name = forms[i].querySelector('input[type="text"]');
+        const button = forms[i].querySelector('input[type="submit"]');
         name.onkeyup = () => {
             if (name.value.length > 0) {
                 button.disabled = false;
@@ -20,23 +24,48 @@ document.addEventListener("DOMContentLoaded", () => {
                 button.disabled = true;
             }
         }
+    }
+
+    if (!handle) {
+        new_user_div.style.display = "block";
+  
+        const new_form = new_user_div.querySelector("form");
+        const name = new_form.querySelector('input[name="handle"]');
+
         new_form.onsubmit = () => {
             //TODO: have to verify that handle is not taken
             handle = name.value;
             localStorage.setItem('user_handle', handle);
             new_user_div.style.display = "none";
-            user_confirmed(old_user_div);
+            user_confirmed();
             return false;
         }
     }
     else {
-        user_confirmed(old_user_div);
+        user_confirmed();
     }
-    
+
+    function user_confirmed()
+    {
+        const chan_create_form = document.getElementById("create-channel");
+        
+        old_user_div.style.display = "block";
+        
+        document.getElementById("welcome-text").innerHTML += handle + "!";
+
+        chan_create_form.onsubmit = () => {
+            const chan_name = document.getElementById("channel-name").value;
+            //console.log(chan_name)
+            socket.emit("create channel", {"channel_name": chan_name});
+        
+            socket.on('resp', data => {
+                console.log(data.reason);
+            });
+
+            return false;
+        }
+
+        
+    }
 })
 
-function user_confirmed(old_user_div)
-{
-    old_user_div.style.display = "block";
-    old_user_div.querySelector("h2").innerHTML += handle + "!";
-}
