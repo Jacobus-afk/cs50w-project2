db4f8c91e48ad700134157a3782830a7598462fb
Cobus <cobus@moduteq.com>
2020-04-22 23:24:13 +0200

    when new channel is created, add it to dropdown menu. Populate channel dropdown menu on socket connection

--- application.py
+++ application.py
@@ -8,17 +8,25 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 channel_list = {}
+#https://stackoverflow.com/questions/53892422/python-ordered-list-of-dictionaries-with-a-size-limit
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@socketio.on("connect")
+def handle_new_connection():
+    for key in channel_list:
+        #print(f"adding {key}")
+        emit("add_channel", {"channelname": key})
+
 @socketio.on("create channel")
 def create_channel(data):
     channelname = data["channel_name"]
     if channelname in channel_list:
-        emit("resp", {"reason": "channel already exists"})
+        return "nack", {"reason": f"channel {channelname} already exists"}
     else:
         channel_list[channelname] = data
-        emit("resp", {"reason": f"channel {channelname} added"})
+        emit("add_channel", {"channelname": channelname}, broadcast=True)
+        return "ack", {"reason": f"channel {channelname} added"}
 
--- static/index.js
+++ static/index.js
@@ -3,13 +3,15 @@ let handle = localStorage.getItem('user_handle');
 document.addEventListener("DOMContentLoaded", () => {
     const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    socket.on('connect', () => {
+    /*socket.on('connect', () => {
         console.log("socket connected")
-    });
+    });*/
 
     const new_user_div = document.getElementById("new-user");
     const old_user_div = document.getElementById("old-user");
-
+    const chan_create_form = document.getElementById("create-channel");
+    const create_msg = document.getElementById("create-msg");
+    const select_chan = document.getElementById("select-channel");
     const forms = document.forms;
     
     for (i = 0; i < forms.length; i++)
@@ -45,27 +47,46 @@ document.addEventListener("DOMContentLoaded", () => {
         user_confirmed();
     }
 
+    function add_channel_selection(name)
+    {
+        const opt = document.createElement('option');
+        opt.value = name;
+        opt.innerHTML = name
+        select_chan.append(opt);
+    }
+    
     function user_confirmed()
     {
-        const chan_create_form = document.getElementById("create-channel");
-        
         old_user_div.style.display = "block";
-        
         document.getElementById("welcome-text").innerHTML += handle + "!";
-
-        chan_create_form.onsubmit = () => {
-            const chan_name = document.getElementById("channel-name").value;
-            //console.log(chan_name)
-            socket.emit("create channel", {"channel_name": chan_name});
-        
-            socket.on('resp', data => {
-                console.log(data.reason);
-            });
-
-            return false;
-        }
-
-        
     }
+
+    chan_create_form.onsubmit = () => {
+        const chan_name = document.getElementById("channel-name").value;
+        //console.log(chan_name)
+        socket.emit("create channel", {"channel_name": chan_name}, (resp, reason) => {
+            if (resp != "ack") {
+                create_msg.style.color = "red";
+            }
+            else {
+                create_msg.style.color = "green";
+            }
+            create_msg.innerHTML = reason.reason;
+            //console.log(resp);
+            //console.log(reason);
+        });
+    
+        //socket.on('resp', data => {
+        //    console.log(data.reason);
+        //});
+
+        return false;
+    }
+
+    socket.on("add_channel", data => {
+        //console.log("add_channel socket call");
+        add_channel_selection(data.channelname);
+    })
+
 })
 
